23cd4c3152705cc8d05b75385a6f1388e545ffe8
Tyler Dillon <harrell_t@icloud.com>
2019-06-05 13:36:28 -0400

    Added pagination and a searchbar
    Added pagination and the beginnings of the search bar

--- js/script.js
+++ js/script.js
@@ -1,50 +1,85 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+// declaring variables
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const ul = document.getElementsByClassName('student-list');
+const li = document.getElementsByClassName('student-item');
+const page = document.getElementById('page');
+const searcher = document.getElementById('search');
+// const top = document.getElementsByClassName('top');
 
 
 
+// shows or hides list items
+const showPage = (list, first, last) => {
+   for (let i=0; i<list.length; i++) {
+      if (i >= first && i <= last) {
+         list[i].style.display = "block";
+      } else {
+         list[i].style.display = "none";
+      }
+   }
+}
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+//adds links to the bottom of the page for pagination and adds functionality to them.
+const appendPageLinks = (list) => {
+   //Create a div, give it the pagination class, and append it to the page div
+   let pagdiv = document.createElement("div");
+   pagdiv.setAttribute('class', 'pagination')
+   page.appendChild(pagdiv);
+   //Add a ul to the pagination div
+   let pagul = document.createElement('ul');
+   pagdiv.appendChild(pagul);
+   //sets page numbers
+   let pagnum = Math.floor(list.length / 10) + 1;
+   for (let i = 0; i < pagnum; i++){
+      //creates each pagination link and content, then appends to the pagination ul
+      let pagli = document.createElement("li");
+      pagli.innerHTML = `<a>${i+1}</a>`;
+      pagul.appendChild(pagli);
+      //sets the first and last list item displayed based on which link is selected
+      let paglink = () => {
+         let first = (i * 10);
+         let last = (i * 10) + 9;
+         //runs the showPage function on our list with the parameters
+         showPage(li, first, last);
+      }
+      //sets up the event listeners for each of the pagination links
+      pagli.addEventListener('click', paglink, false)
+   }
+}
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//interactive searchbar
+const interSearch = () => {
+   let filter = searcher.value.toUpperCase();
+   for (let j = 0; j < li.length; j++) {
+      let h3 = li[j].getElementsByTagName('h3')[0];
+      let txtValue = h3.textContent || h3.innerText;
+      //indexOf returns the index of an item in an array. if not present, it returns -1. 
+      //here I'm asking for the indexOf the thing I typed in the searchbar.
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+         //If the index is greater than -1, display as block
+         li[j].style.display = "block";
+      } else {
+         //if the index is smaller than -1, don't display it.
+         li[j].style.display = "none";
+      }
+   }
+}
 
 
 
+// searcher.addEventListener('input', () => {
+//    if (searcher.value === '') {
+//       for (let k = 0; k < li.length; k++) {
+//          if (k < 10) {
+//             li[k].style.display = "block";
+//          } else {
+//             li[k].style.display = "none";
+//          }
+//       }
+//    }
+// }, false)
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//sets first view of students
+// showPage(li, 0, 9)
+//makes links function
+appendPageLinks(li)
