82099a3af8709da60d913f79263d3bb4af81b399
Tyler Dillon <harrell_t@icloud.com>
2019-06-06 11:17:23 -0400

    Added active class to pagination
    Set the active class for the pagination links and removed pagination when the searchbar was active

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const ul = document.getElementsByClassName('student-list');
 const li = document.getElementsByClassName('student-item');
 const page = document.getElementById('page');
-const searcher = document.getElementById('search');
+const pageHeader = document.getElementById('page-header');
 // const top = document.getElementsByClassName('top');
 
 
@@ -23,17 +23,20 @@ const showPage = (list, first, last) => {
 const appendPageLinks = (list) => {
    //Create a div, give it the pagination class, and append it to the page div
    let pagdiv = document.createElement("div");
-   pagdiv.setAttribute('class', 'pagination')
+   pagdiv.setAttribute('class', 'pagination');
+   pagdiv.setAttribute('id', 'pagdiv');
    page.appendChild(pagdiv);
    //Add a ul to the pagination div
    let pagul = document.createElement('ul');
+   pagul.setAttribute('id', 'pagul')
    pagdiv.appendChild(pagul);
    //sets page numbers
    let pagnum = Math.floor(list.length / 10) + 1;
    for (let i = 0; i < pagnum; i++){
       //creates each pagination link and content, then appends to the pagination ul
       let pagli = document.createElement("li");
-      pagli.innerHTML = `<a>${i+1}</a>`;
+      pagli.setAttribute('class', 'pagli')
+      pagli.innerHTML = `<a class="">${i+1}</a>`;
       pagul.appendChild(pagli);
       //sets the first and last list item displayed based on which link is selected
       let paglink = () => {
@@ -47,9 +50,26 @@ const appendPageLinks = (list) => {
    }
 }
 
+
 //interactive searchbar
+
+//creating the searchbar
+//<input type="text" id="search" class="student-search" onkeyup="interSearch()" placeholder="Search for names..">
+const makeBar = () => {
+   let searchBar = document.createElement('input');
+   searchBar.setAttribute('type', 'text')
+   searchBar.setAttribute('id', 'search')
+   searchBar.setAttribute('class', 'student-search')
+   searchBar.setAttribute('onkeyup', 'interSearch()')
+   searchBar.setAttribute('placeholder', 'Search for names...')
+   pageHeader.appendChild(searchBar)
+}
+
+// adapted from: https://www.w3schools.com/howto/howto_js_filter_lists.asp
 const interSearch = () => {
+   const searcher = document.getElementById('search');
    let filter = searcher.value.toUpperCase();
+   let pagdiv = document.getElementById('pagdiv')
    for (let j = 0; j < li.length; j++) {
       let h3 = li[j].getElementsByTagName('h3')[0];
       let txtValue = h3.textContent || h3.innerText;
@@ -61,19 +81,41 @@ const interSearch = () => {
             } else {
                li[k].style.display = "none";
             }}
+            pagdiv.style.display = "block"
       //indexOf returns the index of an item in an array. if item is not present, it returns -1. 
       //here I'm asking for the indexOf the thing I typed in the searchbar.
       } else if (txtValue.toUpperCase().indexOf(filter) > -1) {
          //If the index is greater than -1, display it
          li[j].style.display = "block";
+         pagdiv.style.display = "none";
       } else {
          //if the index is smaller than -1, don't display it.
          li[j].style.display = "none";
+         pagdiv.style.display = "none";
       }
    }
 }
 
 //sets first view of students
 showPage(li, 0, 9)
+makeBar()
 //makes links function
 appendPageLinks(li)
+
+//declares pagul and pagli for event listener
+const pagul = document.getElementById('pagul');
+const pagli = document.getElementsByClassName('pagli');
+const list = pagul.querySelectorAll('a');
+list[0].setAttribute('class', 'active')
+//active links
+const setActClass = (e) => {
+   //selects the a elements from pagination links
+   for (let i = 0; i < pagli.length; i++) {
+      //clears the class from pagination links
+      list[i].removeAttribute('class')
+   }
+   //sets the active class to the selected link
+   e.target.setAttribute('class', 'active');
+}
+//calls the event listener on the pagination links for active class
+pagul.addEventListener('click', setActClass, false);
